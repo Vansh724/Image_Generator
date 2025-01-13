b6748a34ce09ca33365b049cbaf456737aa2b30b
Dhruvi-Khandelwal <dhruvikhandelwal10@gmail.com>
2025-01-14 01:00:01 +0530

    Learning Setting Page

--- app/src/main/java/com/example/aiimagegenerator/SettingsPage.java
+++ app/src/main/java/com/example/aiimagegenerator/SettingsPage.java
@@ -1,6 +1,10 @@
 package com.example.aiimagegenerator;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,25 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SettingsPage extends AppCompatActivity {
+    Button Hist;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_settings_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Hist = findViewById(R.id.button);
+        Hist.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(SettingsPage.this, "Successful", Toast.LENGTH_SHORT).show();
+
+                    Intent hist = new Intent(SettingsPage.this, HistoryPage.class);
+                    startActivity(hist);
+                }
+            });
+
     }
 }
